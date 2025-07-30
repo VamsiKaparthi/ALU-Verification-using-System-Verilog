4e3c0a1be633d7c8e60a5c3d91071fffb00ea13d
VamsiKaparthi <89274263+VamsiKaparthi@users.noreply.github.com>
2025-07-30 14:32:27 +0530

    Update monitor.sv

--- src/monitor.sv
+++ src/monitor.sv
@@ -1,9 +1,13 @@
 class monitor;
-        transaction pkt, pkt1;
+        transaction pkt;
         mailbox #(transaction) mms; //monitor to scoreboard mailbox
         virtual alu_if vif;
         covergroup mon_cg; //output coverage
-                c1 : coverpoint pkt.res;
+                c1 : coverpoint pkt.res{
+                        bins b1 = {0};
+                        bins b2 = {511};
+                        bins b3 = default;
+                }
                 c2 : coverpoint pkt.g;
                 c3 : coverpoint pkt.l;
                 c4 : coverpoint pkt.e;
@@ -24,55 +28,64 @@ class monitor;
                 pkt.mode = vif.mode;
                 pkt.inp_valid = vif.inp_valid;
                 pkt.cin = vif.cin;
-                $display("[%0t]--Monitor in interface -- inp_valid = %0d, opa = %0d, opb = %0d", $time, vif.inp_valid, vif.opa, vif.opb);
-
-        endtask
-        task send();
-                pkt1.res = vif.res;
-                pkt1.cout = vif.cout;
-                pkt1.err = vif.err;
-                pkt1.oflow = vif.oflow;
-                pkt1.g = vif.g;
-                pkt1.l = vif.l;
-                pkt1.e = vif.e;
+                pkt.res = vif.res;
+                pkt.cout = vif.cout;
+                pkt.err = vif.err;
+                pkt.oflow = vif.oflow;
+                pkt.g = vif.g;
+                pkt.l = vif.l;
+                pkt.e = vif.e;
+                //$display("[%0t]--Monitor in interface -- inp_valid = %0d, opa = %0d, opb = %0d", $time, vif.inp_valid, vif.opa, vif.opb);
+                mon_cg.sample;
         endtask
         task start();
+                //bit flag = 0;
                 int single_op_arithmetic[] = {4,5,6,7};
                 int single_op_logical[] = {6,7,8,9,10,11};
                 pkt = new(); //for recieving from DUT
-                pkt1 = new();
-                repeat(3)@(vif.mon_cb); // 1 cycle delay to driver
+                repeat(4)@(vif.mon_cb); // 1 cycle delay to driver
                 repeat(num)begin
+                //      flag = 0;
+                        //@(vif.mon_cb);
+                        pkt = new();
                         recieve();
-                        $display("[%0t]--Monitor-- inp_valid = %0d, opa = %0d, opb = %0d", $time, pkt.inp_valid, pkt.opa, pkt.opb);
-                        @(vif.mon_cb);
-                        if(pkt.inp_valid == 2'b11 || pkt.inp_valid == 2'b01)begin
-                                send();
-                                if(pkt.inp_valid == 2'b11 && (pkt.cmd == 4'b1010 || pkt.cmd == 4'b1001))
+                        $display("[%0t] THE INITIAL IP PKT IN MONITOR IS inp_valid = %0d, opa = %0d, opb = %0d", $time,pkt.inp_valid, pkt.opa, pkt.opb);
+                        if(pkt.inp_valid == 2'b11 || pkt.inp_valid == 2'b00)begin
+                                recieve();
+                                if(pkt.inp_valid == 2'b11 && (pkt.cmd == 4'b1010 || pkt.cmd == 4'b1001)&& pkt.mode == 1)begin
+                                        $display("[%0t] ---Monitor--- pkt.res = %0d, pkt.err = %0b", $time, pkt.res, pkt.err);
                                         repeat(2)@(vif.mon_cb);
-                                else
+                                        recieve();
+                                end
+                                else begin
                                         repeat(1)@(vif.mon_cb);
-                                mms.put(pkt1);
+                                        $display("[%0t] ---Monitor--- pkt.res = %0d, pkt.err = %0b", $time, pkt.res, pkt.err);
+                                        recieve();
+                                end
+                                $display("[%0t] ---Monitor_Outputs--- res = %0d, err = %0b", $time, pkt.res, pkt.err);
+
+                                //mms.put(pkt);
                         end
                         else begin
                                 if(pkt.mode == 0)begin
                                         if(pkt.cmd inside {single_op_logical})begin
-
                                                 repeat(1)@(vif.mon_cb);
-                                                send();
-                                                mms.put(pkt1);
-                                                $display("[%0t] ---Monitot--- pkt.res = %0d", $time, pkt1.res);
+                                                recieve();
+                                                //mms.put(pkt);
+                                                $display("[%0t] ---Monitor_Outputs--- res = %0d, err = %0b", $time, pkt.res, pkt.err);
                                         end
                                         else begin
-                                                repeat(1)@(vif.mon_cb);
-
+                                                //repeat(1)@(vif.mon_cb);
+                                                recieve();
                                                 for(int i = 0; i < 16; i++)begin
-                                                        @(vif.mon_cb);
+                                                        repeat(1)@(vif.mon_cb);
                                                         recieve();
-                                                        send();
-                                                        $display("[%0t] ---Monitor--- pkt.res = %0d", $time, pkt1.res);
+                                                        $display("[%0t] ---Monitor--- pkt.inp_valid = %0d", $time, pkt.inp_valid);
                                                         if(pkt.inp_valid == 2'b11)begin
-                                                                mms.put(pkt1);
+                                                //              @(vif.mon_cb);
+                                                                //mms.put(pkt);
+                                                                $display("[%0t] ---Monitor_Outputs--- res = %0d, err = %0b", $time, pkt.res, pkt.err);
+
                                                                 break;
                                                         end
                                                 end
@@ -81,23 +94,31 @@ class monitor;
                                 if(pkt.mode == 1)begin
                                         if(pkt.cmd inside {single_op_arithmetic})begin
                                                 repeat(1)@(vif.mon_cb);
-                                                send();
-                                                mms.put(pkt1);
-                                                $display("%t pkt.res = %0d", $time, pkt1.res);
+                                                recieve();
+                                                //mms.put(pkt);
+                                                $display("[%0t] --Monitor-- pkt.res = %0d", $time, pkt.res);
                                         end
                                         else begin
-                                                repeat(1)@(vif.mon_cb);
-                                                $display("%t pkt.res = %0d", $time, pkt1.res);
+                                                //repeat(1)@(vif.mon_cb);
+                                                recieve();
+                                                $display("[%0t] --Monitor-- inp_valid = %0d, opa = %0d, opb = %0d", $time, pkt.inp_valid, pkt.opa, pkt.opb);
                                                 for(int i = 0; i < 16; i++)begin
                                                         @(vif.mon_cb);
                                                         recieve();
+                                                        $display("[%0t] --Monitor-- inp_valid = %0d, opa = %0d, opb = %0d", $time, pkt.inp_valid, pkt.opa, pkt.opb);
                                                         if(pkt.inp_valid == 2'b11)begin
                                                                 if(pkt.cmd == 4'b1010 || pkt.cmd == 4'b1001)begin
                                                                         repeat(1)@(vif.mon_cb);
+                        //                                              repeat(1)@(vif.mon_cb);
+                                                                        $display("[%0t] ---Monitor_Outputs--- res = %0d, err = %0b, opa = %0d, opb = %0d", $time, pkt.res, pkt.err, pkt.opa, pkt.opb);
+                //                                                      flag = 1;
                                                                         break;
                                                                 end
                                                                 else begin
-                                                                        repeat(0)@(vif.mon_cb)
+                                                                        repeat(0)@(vif.mon_cb);
+                        //                                              repeat(1)@(vif.mon_cb);
+                                                                         $display("[%0t] ---Monitor_Outputs [inside loop]--- res = %0d, err = %0b", $time, pkt.res, pkt.err);
+                //                                                      flag = 1;
                                                                         break;
                                                                 end
                                                         end
@@ -106,9 +127,27 @@ class monitor;
 
                                 end
                         end
-                        if(pkt.cmd == 4'b1010 || pkt.cmd == 4'b1001)
+                //      if(flag == 0)
+                //              @(vif.mon_cb);
+                        recieve();
+                        //$display("[%0t] ##-Monitor_Outputs--- res = %0d, err = %0b, opa = %0d, opb = %0d", $time, pkt.res, pkt.err, pkt.opa, pkt.opb);
+
+                        //mms.put(pkt);
+                        if((pkt.cmd == 4'b1010 || pkt.cmd == 4'b1001) && pkt.mode)begin
                                 @(vif.mon_cb);
+                                pkt.err <= vif.err;
+                        end
                         @(vif.mon_cb);
+        //              recieve();
+                        pkt.err <= vif.err;
+                        pkt.cout <= vif.cout;
+                        pkt.res <= vif.res;
+                        pkt.g <= vif.g;
+                        pkt.l <= vif.l;
+                        pkt.e <= vif.e;
+                        pkt.oflow <= vif.oflow;
+                        $display("[%0t] ##-Monitor_Outputs--- res = %0d, err = %0b, opa = %0d, opb = %0d", $time, pkt.res, pkt.err, pkt.opa, pkt.opb);
+                        mms.put(pkt);
                 end
         endtask
 endclass
