f0f5f4dec71e86c5c237f963673350a1b38ec792
VamsiKaparthi <89274263+VamsiKaparthi@users.noreply.github.com>
2025-07-30 14:36:26 +0530

    Create pkg.sv

---
+++ src/pkg.sv
@@ -0,0 +1,11 @@
+package pkg;
+        `include "define.svh"
+        `include "transaction.sv"
+        `include "generator.sv"
+        `include "driver.sv"
+        `include "reference.sv"
+        `include "monitor.sv"
+        `include "scoreboard.sv"
+        `include "environment.sv"
+        `include "test.sv"
+endpackage
