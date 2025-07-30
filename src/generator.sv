e76d1ad9b83c36028403c9b6be0ce59273304087
VamsiKaparthi <89274263+VamsiKaparthi@users.noreply.github.com>
2025-07-30 14:31:18 +0530

    Update generator.sv

--- src/generator.sv
+++ src/generator.sv
@@ -3,14 +3,16 @@ class generator;
         transaction pkt;
         function new(mailbox #(transaction) mgd);
                 this.mgd = mgd;
+                pkt = new();
         endfunction
         task start();
-                pkt = new();
+                //pkt = new();
                 repeat(num)begin
                         void'(pkt.randomize());
                         mgd.put(pkt.copy());
                         $display("\nThe Packet is generated with data : ");
                         $display("opa = %d | opb = %d | cin = %d | inp_valid = %d | mode = %d | cmd = %d", pkt.opa, pkt.opb, pkt.cin, pkt.inp_valid, pkt.mode, pkt.cmd);
                 end
+                $display("-------------------------------------------------------");
         endtask
 endclass
